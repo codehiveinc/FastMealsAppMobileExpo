54ab1688aa3c6778947d6b9c26b672929e2e5096
Rodrigo Ruiz García <rodrick@MacBook-Air-de-Rodrigo.local>
2024-06-26 14:43:58 -0600

    initial design of authscreen

--- src/auth/infrastructure/ui/screens/AuthScreen.tsx
+++ src/auth/infrastructure/ui/screens/AuthScreen.tsx
@@ -222,18 +222,14 @@ const styles = StyleSheet.create({
     paddingTop: 120,
     borderBottomLeftRadius: 30,
     borderBottomRightRadius: 30,
-    shadowOpacity: 0.06,
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowRadius: 30,
-    elevation: 5,
+
+          
     shadowColor: "#000",
     width: "100%",
   },
   iconWrapper: {
     backgroundColor: "#fff",
+
     borderRadius: 50,
     padding: 20,
     marginBottom: 10,
@@ -269,6 +265,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 4,
     borderBottomColor: "#0C9488",
     marginLeft: 60,
+
   },
   input: {
     width: 314,
@@ -285,6 +282,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     fontWeight: "bold",
     fontSize: 17,
+
   },
   loginButton: {
     width: 314,
