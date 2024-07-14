ee101cd6e5b0594152e8182fdeba2768a163915c
Fernando Guerrero <devrrior@gmail.com>
2024-07-14 17:33:20 -0600

    fix(MainNavigator): Remove wrong comments

--- src/shared/infrastructure/navigation/MainNavigatior.tsx
+++ src/shared/infrastructure/navigation/MainNavigatior.tsx
@@ -8,7 +8,6 @@ import PrivacyPolicyScreen from "@/users/infrastructure/ui/screens/PrivacyPolicy
 import RestaurantScreen from "@/restaurants/infrastructure/ui/screens/RestaurantScreen";
 import MealDetailScreen from "@/restaurants/infrastructure/ui/screens/MealDetailScreen";
 import OrderScreen from "@/orders/infrastructure/ui/Screens/OrderScreen";
-<<<<<<< HEAD
 import UpdateMealScreen from "@/restaurants/infrastructure/ui/screens/UpdateMealScreen";
 import CreateMealScreen from "@/restaurants/infrastructure/ui/screens/CreateMealScreen";
 import UpdateUserScreen from "@/users/infrastructure/ui/UpdateUserScreen";
@@ -17,10 +16,8 @@ import MyRestaurantScreen from "@/users/infrastructure/ui/screens/MyRestaurantSc
 import EditMealScreen from "@/users/infrastructure/ui/screens/EditMealScreen";
 import PaymentsScreen from "@/payment/infrastructure/ui/screens/PaymentsScreen";
 import PaymentMethodScreen from "@/payment/infrastructure/ui/screens/PaymentMethodScreen";
-=======
 import MyRestaurantsScreen from "@/restaurants/infrastructure/ui/screens/MyRestaurantsScreen";
 import RootStackParamList from "../ui/types/RootStackParamList";
->>>>>>> 25dad25 (feat(MyRestaurants): Add MyRestaurantsScreen)
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -42,14 +39,23 @@ const MainNavigator = () => {
         <Stack.Screen name="RestaurantScreen" component={RestaurantScreen} />
         <Stack.Screen name="MealDetailScreen" component={MealDetailScreen} />
         <Stack.Screen name="OrderScreen" component={OrderScreen} />
-        <Stack.Screen name="UpdateMealScreen" component={UpdateMealScreen}/>
-        <Stack.Screen name="CreateMealScreen" component={CreateMealScreen}/>
-        <Stack.Screen name="UpdateUserScreen" component={UpdateUserScreen}/>
-        <Stack.Screen name="CreateRestaurantScreen" component={CreateRestaurantScreen}/>
-        <Stack.Screen name="MyRestaurantScreen" component={MyRestaurantScreen} />
+        <Stack.Screen name="UpdateMealScreen" component={UpdateMealScreen} />
+        <Stack.Screen name="CreateMealScreen" component={CreateMealScreen} />
+        <Stack.Screen name="UpdateUserScreen" component={UpdateUserScreen} />
+        <Stack.Screen
+          name="CreateRestaurantScreen"
+          component={CreateRestaurantScreen}
+        />
+        <Stack.Screen
+          name="MyRestaurantScreen"
+          component={MyRestaurantScreen}
+        />
         <Stack.Screen name="EditMealScreen" component={EditMealScreen} />
         <Stack.Screen name="PaymentsScreen" component={PaymentsScreen} />
-        <Stack.Screen name="PaymentMethodScreen" component={PaymentMethodScreen} />
+        <Stack.Screen
+          name="PaymentMethodScreen"
+          component={PaymentMethodScreen}
+        />
         <Stack.Screen
           name="MyRestaurantsScreen"
           component={MyRestaurantsScreen}
