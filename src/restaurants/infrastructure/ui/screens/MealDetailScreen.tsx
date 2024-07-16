5ae088f31b52bb9eb13f5dc8581330935f9c483e
Fernando Guerrero <devrrior@gmail.com>
2024-07-16 02:46:06 -0600

    refactor(ProductDetailScreen): Refactor old code

--- src/restaurants/infrastructure/ui/screens/MealDetailScreen.tsx
+++ src/restaurants/infrastructure/ui/screens/MealDetailScreen.tsx
@@ -1,109 +1,98 @@
-import React from 'react';
-import { SafeAreaView, View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import React from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
-import MealImage from 'assets/images/MealImage.png'
-import AppBar from '@/shared/infrastructure/ui/components/AppBar';
-import { MealDetailScreenRouteProps } from '@/restaurants/infrastructure/ui/types/RestaurantsScreensRouteProps';
-const MealDetailScreen = ({ navigation }:MealDetailScreenRouteProps ) => {
+import MealImage from "assets/images/MealImage.png";
+import AppBar from "@/shared/infrastructure/ui/components/AppBar";
+import { MealDetailScreenRouteProps } from "@/restaurants/infrastructure/ui/types/RestaurantsScreensRouteProps";
+import BasicLayout from "@/shared/infrastructure/ui/layouts/BasicLayout";
+import { colors } from "@/shared/infrastructure/ui/consts/colors";
+import { fonts } from "@/shared/infrastructure/ui/consts/fonts";
+import Button from "@/shared/infrastructure/ui/components/Button";
+const MealDetailScreen = ({ navigation }: MealDetailScreenRouteProps) => {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <AppBar 
-        leftIcon="LeftIcon"
+    <BasicLayout>
+      <AppBar
+        leftIcon="chevron-back"
         onLeftPress={() => navigation.goBack()}
-        title='Detalles del platillo'
+        title="Detalles del platillo"
       />
 
-      
       <View style={styles.container}>
         <Image
           style={styles.image}
-          source={MealImage} // reemplaza con la URL de tu imagen
+          source={{ uri: "https://via.placeholder.com/300" }} // reemplaza con la URL de tu imagen
         />
-        <Text style={styles.title}>Veggie tomato mix</Text>
-        <Text style={styles.price}>N1,900</Text>
+        <Text style={styles.title}>Veggie tomato mifasd asdf dsafx</Text>
+        <Text style={styles.price}>$1,900</Text>
         <View style={styles.infoContainer}>
           <Text style={styles.infoTitle}>Información de entrega</Text>
-          <Text style={styles.infoText}>Entregas de Lunes a Domingo de 10 am a 10 pm</Text>
+          <Text style={styles.infoText}>
+            Entregas de Lunes a Domingo de 10 am a 10 pm
+          </Text>
         </View>
         <View style={styles.infoContainer}>
           <Text style={styles.infoTitle}>Política de retorno</Text>
-          <Text style={styles.infoText}>Si el producto se encuentra en mal estado se aplica la política de retorno</Text>
+          <Text style={styles.infoText}>
+            Si el producto se encuentra en mal estado se aplica la política de
+            retorno
+          </Text>
         </View>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Añadir al carro</Text>
-        </TouchableOpacity>
       </View>
-    </SafeAreaView>
+      <Button
+        text="Agregar al carrito"
+        handlePress={() => navigation.navigate("Cart")}
+        backgroundColor={colors.primary}
+        textColor={colors.white}
+        width={"100%"}
+      />
+    </BasicLayout>
   );
 };
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop : 30 
-  },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    paddingTop: 50,
-  },
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     padding: 10,
-    marginHorizontal: 20,
+    gap: 25,
   },
   image: {
     width: 250,
     height: 250,
     borderRadius: 75,
-    marginBottom: 5,
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    textAlign: 'center',
+    fontFamily: fonts.primary,
+    fontSize: 28,
+    textAlign: "center",
   },
   price: {
+    fontFamily: fonts.primary,
     fontSize: 18,
-    fontWeight: 'bold',
-    color: '#000',
-    marginBottom: 20,
-    textAlign: 'center',
+    fontWeight: "bold",
+    color: colors.black,
+    textAlign: "center",
   },
   infoContainer: {
-    marginBottom: 10,
-    width: '100%',
+    width: "100%",
   },
   infoTitle: {
     fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 5,
-    textAlign: 'justify',
+    fontWeight: "bold",
+    textAlign: "justify",
   },
   infoText: {
-    fontSize: 14,
-    color: '#666',
-    textAlign: 'justify',
-  },
-  button: {
-    backgroundColor: '#0C9488',
-    paddingVertical: 15,
-    paddingHorizontal: 70,
-    borderRadius: 30,
-    marginTop: 70,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontFamily: fonts.primary,
+    fontSize: 15,
+    color: colors.gray,
+    textAlign: "justify",
   },
 });
 
