f8e2199397e0d0141a10169c90bb031bd4d237cb
Fernando Guerrero <devrrior@gmail.com>
2024-07-16 10:49:43 -0600

    refactor(MyRestaurantsScreen): Extract components

--- src/restaurants/infrastructure/ui/screens/MyRestaurantsScreen.tsx
+++ src/restaurants/infrastructure/ui/screens/MyRestaurantsScreen.tsx
@@ -1,23 +1,48 @@
 import AppBar from "@/shared/infrastructure/ui/components/AppBar";
-import {
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView, StyleSheet } from "react-native";
 import { MyRestaurantsScreenRouteProps } from "../types/RestaurantsScreensRouteProps";
-import { Ionicons } from "@expo/vector-icons";
+import BasicLayout from "@/shared/infrastructure/ui/layouts/BasicLayout";
+import RestaurantOption from "../components/RestaurantOption";
+
+const restaurants = [
+  {
+    id: "1",
+    name: "Restaurante los aguacates asdf dsaf",
+    image: "https://via.placeholder.com/350",
+  },
+  {
+    id: "2",
+    name: "Restaurante los aguacates asdf dsaf",
+    image: "https://via.placeholder.com/350",
+  },
+  {
+    id: "3",
+    name: "Restaurante los aguacates asdf dsaf",
+    image: "https://via.placeholder.com/350",
+  },
+  {
+    id: "4",
+    name: "Restaurante los aguacates asdf dsaf",
+    image: "https://via.placeholder.com/350",
+  },
+  {
+    id: "5",
+    name: "Restaurante los aguacates asdf dsaf",
+    image: "https://via.placeholder.com/350",
+  },
+];
 
 const MyRestaurantsScreen = ({ navigation }: MyRestaurantsScreenRouteProps) => {
   const goBack = () => {
     navigation.goBack();
   };
 
+  const handlePressRestaurant = () => {
+    navigation.navigate("MyRestaurantScreen");
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
+    <BasicLayout>
       <AppBar
         leftIcon="chevron-back"
         onLeftPress={() => goBack()}
@@ -28,65 +53,22 @@ const MyRestaurantsScreen = ({ navigation }: MyRestaurantsScreenRouteProps) => {
         contentContainerStyle={styles.scrollViewContainer}
         style={{ overflow: "visible" }}
       >
-        <View style={styles.restaurantItemContainer}>
-          <Image
-            style={styles.restaurantImage}
-            source={{ uri: "https://via.placeholder.com/350" }}
+        {restaurants.map((restaurant) => (
+          <RestaurantOption
+            key={restaurant.id}
+            handlePress={handlePressRestaurant}
+            restaurantName={restaurant.name}
+            restaurantImageUrl={restaurant.image}
           />
-          <Text style={styles.restaurantTitle}>
-            Restaurante los aguacates asdf dsaf
-          </Text>
-          <TouchableOpacity>
-            <Ionicons name="chevron-forward" size={20} color="black" />
-          </TouchableOpacity>
-        </View>
+        ))}
       </ScrollView>
-    </SafeAreaView>
+    </BasicLayout>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    gap: 30,
-    paddingHorizontal: 30,
-    backgroundColor: "#F5F5F8",
-    width: "100%",
-    height: "100%",
-  },
   scrollViewContainer: {
     gap: 20,
-  },
-  restaurantItemContainer: {
-    gap: 10,
-    width: "100%",
-    paddingVertical: 20,
-    paddingHorizontal: 15,
-    backgroundColor: "#fff",
-    borderRadius: 20,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 0.25,
-  },
-  restaurantInfoContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 10,
-  },
-  restaurantImage: {
-    borderRadius: 99,
-    width: "16%",
-    aspectRatio: 1,
-  },
-  restaurantTitle: {
-    fontFamily: "Poppins-Regular",
-    fontSize: 18,
-    flex: 1,
   },
 });
 
